326e707bb6d1626375d6864ba8cc4ca8c7b6899b
Sarthak Narayan <42849256+SarthakNarayan@users.noreply.github.com>
2023-09-25 12:14:44 -0400

    added vim mode to zsh and updated readme

--- ezshrc.zsh
+++ ezshrc.zsh
@@ -121,7 +121,8 @@ plugins=(
 
 # Add to PATH to Install and run programs with "pip install --user"
 export PATH=$PATH:~/.local/bin
-
+export EDITOR="vim"
+export VISUAL="vim"
 export PATH=$PATH:~/.config/ezsh/bin
 
 NPM_PACKAGES="${HOME}/.npm"
@@ -136,5 +137,57 @@ autoload -U compinit && compinit -C -d ~/.cache/zsh/.zcompdump        # zsh-comp
 SAVEHIST=50000      #save upto 50,000 lines in history. oh-my-zsh default is 10,000
 #setopt hist_ignore_all_dups     # dont record duplicated entries in history during a single session
 
+# Aliases
 alias myip="wget -qO- https://wtfismyip.com/text"	# quickly show external ip address
-alias e="exit"
+alias c='clear -x'
+alias history="history -t '%F'"
+alias cp="cp -v"
+alias mv="mv -v"
+alias path="print -l $path" #print path in a human readable way
+
+# change it to true if you want to use vim mode in terminal
+export VIM_MODE=false
+
+if $VIM_MODE; then
+	echo "VIM_MODE is true"
+    bindkey -v
+
+    # if you have a low keytimeout value it is not possible to use jk for escaping from insert mode
+    # https://superuser.com/questions/351499/how-to-switch-comfortably-to-vi-command-mode-on-the-zsh-command-line
+    export KEYTIMEOUT=20 
+
+    # Use vim keys in tab complete menu:
+    bindkey -M menuselect 'h' vi-backward-char
+    bindkey -M menuselect 'k' vi-up-line-or-history
+    bindkey -M menuselect 'l' vi-forward-char
+    bindkey -M menuselect 'j' vi-down-line-or-history
+    bindkey -M menuselect '^[[Z' vi-up-line-or-history
+
+    bindkey -v '^?' backward-delete-char # using backspace to delete
+    bindkey -M viins 'jk' vi-cmd-mode # remaping esc to jk
+
+    # move to beginning and end of line
+    # https://stackoverflow.com/questions/18042685/list-of-zsh-bindkey-commands
+    # https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html
+    bindkey -M vicmd 'H' beginning-of-line
+    bindkey -M vicmd 'L' end-of-line
+
+    # Change cursor shape for different vi modes.
+    function zle-keymap-select () {
+        case $KEYMAP in
+            vicmd) echo -ne '\e[1 q';;      # block
+            viins|main) echo -ne '\e[5 q';; # beam
+        esac
+    }
+    zle -N zle-keymap-select
+    zle-line-init() {
+        zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+        echo -ne "\e[5 q"
+    }
+    zle -N zle-line-init
+    echo -ne '\e[5 q' # Use beam shape cursor on startup.
+    preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+else
+	echo "VIM_MODE is false"
+fi
