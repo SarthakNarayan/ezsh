a88c8995e7e0cf83d2a4664fa3bc7a0fa8e6b0b4
Sarthak Narayan <sarthak.narayang@gmail.com>
2024-08-17 16:25:29 -0400

    adding fzffind alias to aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -9,3 +9,4 @@ alias path="print -l $path" #print path in a human readable way
 alias sc="source ~/.zshrc"
 alias rc="vim ~/.config/ezsh/ezshrc.zsh"
 alias vim="nvim"
+alias fzffind="fzf --preview='bat --color=always'"
